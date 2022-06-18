45884ce787c0e1cdeb82f58ff4ea0f57fba99f74
ChanAlvin <67116205+ChanAlvin@users.noreply.github.com>
2022-06-18 19:51:03 -0400

    Bishop check code added
    Alvin Chan - June 18, 2022
    Took move code from earlier bishop move code and copy pasta
    Gutted move code and inserted code to search for king in its path

--- a21-culminating-project/src/beta_versions/BishopProperties.java
+++ a21-culminating-project/src/beta_versions/BishopProperties.java
@@ -1,6 +1,8 @@
 package beta_versions;
 
 public class BishopProperties {
+	final int BOARD_SIZE = 8;
+	
 	public void ruleSet(String[][] chessBoardMap, String charTurnContain, int posX, int posY, final int BOARD_SIZE) {
 		int j = 0;
 		
@@ -90,6 +92,75 @@ public class BishopProperties {
 	}
 	
 	public boolean gameCheck(String[][] chessBoardMap, int posX, int posY) {
+		int j = 0;
+		
+		if (chessBoardMap[posX][posY].equals("wB")) {
+			j = posY + 1;
+			for (int i = posX + 1; i < BOARD_SIZE; i++) {
+				if (chessBoardMap[i][j].contains("bK")) {
+					return true;
+				}
+				j++;
+			}
+
+			j = posY - 1;
+			for (int i = posX - 1; i > -1; i--) {
+				if (chessBoardMap[i][j].contains("bK")) {
+					return true;
+				}
+				j--;
+			}
+
+			j = posY + 1;
+			for (int i = posX - 1; i > -1; i--) {
+				if (chessBoardMap[i][j].contains("bK")) {
+					return true;
+				}
+				j++;
+			}
+
+			j = posY - 1;
+			for (int i = posX + 1; i < BOARD_SIZE; i++) {
+				if (chessBoardMap[i][j].contains("bK")) {
+					return true;
+				}
+				j--;
+			}
+		}
+		
+		if (chessBoardMap[posX][posY].equals("bB")) {
+			j = posY + 1;
+			for (int i = posX + 1; i < BOARD_SIZE; i++) {
+				if (chessBoardMap[i][j].contains("wK")) {
+					return true;
+				}
+				j++;
+			}
+
+			j = posY - 1;
+			for (int i = posX - 1; i > -1; i--) {
+				if (chessBoardMap[i][j].contains("wK")) {
+					return true;
+				}
+				j--;
+			}
+
+			j = posY + 1;
+			for (int i = posX - 1; i > -1; i--) {
+				if (chessBoardMap[i][j].contains("wK")) {
+					return true;
+				}
+				j++;
+			}
+
+			j = posY - 1;
+			for (int i = posX + 1; i < BOARD_SIZE; i++) {
+				if (chessBoardMap[i][j].contains("wK")) {
+					return true;
+				}
+				j--;
+			}
+		}
 		return false;
 	}
 	
